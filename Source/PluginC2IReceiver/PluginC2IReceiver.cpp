fc4766c58690e65937d51e02ee3c8e6bb557fe2b
fweidner <info@florianweidner.de>
2017-05-28 10:58:59 +0200

    Moved .git folder.

--- Source/PluginC2IReceiver/PluginC2IReceiver.cpp
+++
@@ -1,5 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "PluginC2IReceiver.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PluginC2IReceiver, "PluginC2IReceiver" );
--- Source/PluginC2IReceiver/PluginC2IReceiverGameModeBase.cpp
+++
@@ -1,8 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "PluginC2IReceiver.h"
-#include "PluginC2IReceiverGameModeBase.h"
-
-
-
-
